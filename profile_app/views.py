cdef99532bb16e51eba09f0542d5efb5891e37ab
Muhammad Muneeb ur Rehman <m.muneeb.ur.rehman.2000@gmail.com>
2024-07-14 12:41:01 +0500

    Api Access Permissions Handled.

--- profile_app/views.py
+++ profile_app/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
-from profile_app import serializers, models
+from rest_framework.authentication import TokenAuthentication
+from profile_app import serializers, models, permissions
 
 class UserProfileViewSet(ModelViewSet):
     serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
-    
+    permission_classes = (permissions.UpdateOwnProfile,)
+    authentication_classes = (TokenAuthentication,)
